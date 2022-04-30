a78b5ce6a23dba47508498348f97e4a931601fae
Fikayo Adepoju <fikfikky@gmail.com>
2022-04-30 20:34:16 +0100

    Load test edit

--- loadtest/index.js
+++ loadtest/index.js
@@ -4,10 +4,10 @@ const autocannon = require('autocannon');
 
 autocannon({
     url : "http://localhost:1337",
-    amount: 300,
+    amount: 3000,
     connections: 10, //default
     pipelining: 1, // default
-    duration: 10,
+    duration: 300,
     requests: [
       {
         method: 'POST', // POST for creating a product
@@ -21,3 +21,25 @@ autocannon({
       }
     ]
   }, console.log)
+  
+
+  /*autocannon({
+    url : "https://8qecudppu0.execute-api.us-east-2.amazonaws.com/live",
+    amount: 3000,
+    connections: 10, //default
+    pipelining: 1, // default
+    duration: 300,
+    requests: [
+      {
+        method: 'POST', // POST for creating a product
+        path: '/webhooksolutionapi',
+        body: JSON.stringify({ name: Date.now().toString(36) }),
+        onResponse: (status, body, context) => {
+          if (status === 200) {
+            //context.product = JSON.parse(body)
+          } // on error, you may abort the benchmark
+        }
+      }
+    ]
+  }, console.log)
+  */
